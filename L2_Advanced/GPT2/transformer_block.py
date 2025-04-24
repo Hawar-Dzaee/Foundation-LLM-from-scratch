a7c9244c845bf67df0e9b82343faeefee86068ca
Hawar-Dzaee <hawardizayee@gmail.com>
2025-04-24 10:59:20 -0400

    adding transformer block

---
+++ L2_Advanced/GPT2/transformer_block.py
@@ -0,0 +1,47 @@
+import torch 
+from torch import nn 
+
+class TransformerBlock(nn.Module):
+    def __init__(self,config):
+        super().__init__()
+        self.ln1 = nn.LayerNorm(config['embedding_dim'])
+        self.mha = nn.MultiheadAttention(
+            embed_dim= config['embedding_dim'],
+            num_heads= config['num_heads'],
+            batch_first= config['batch_first'],
+            bias= config['Q_K_V_bias'],
+            add_bias_kv= config['kv_bias'],
+            dropout= config['dropout'],
+            device= config['device']
+            )
+        self.dp1 = nn.Dropout(config['dropout'])
+
+        self.ln2 = nn.LayerNorm(config['embedding_dim'])
+        self.mlp = nn.Sequential(
+            nn.Linear(config['embedding_dim'],2*config['embedding_dim']),
+            nn.GELU(),
+            nn.Linear(2*config['embedding_dim'],config['embedding_dim']),
+        )
+        self.dp2 = nn.Dropout(config['dropout'])
+
+
+    def forward(self,x):
+       _,num_tokens,_ = x.shape
+
+       shortcut = x
+       x = self.ln1(x)
+       x,_ = self.mha(
+           x,x,x,
+           attn_mask= torch.triu(torch.ones(num_tokens,num_tokens),diagonal=1).bool())
+       x = self.dp1(x)
+
+       x = x + shortcut
+
+       shortcut = x
+       x = self.ln2(x)
+       x = self.mlp(x)
+       x = self.dp2(x)
+       x = x + shortcut
+       return x
+       
+
