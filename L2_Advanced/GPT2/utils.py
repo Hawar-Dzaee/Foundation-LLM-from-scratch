940f722793bfc717c0232cef70d19ad07cd2c771
Hawar-Dzaee <hawardizayee@gmail.com>
2025-04-26 16:55:22 -0400

    added generate_text function

--- L2_Advanced/GPT2/utils.py
+++ L2_Advanced/GPT2/utils.py
@@ -24,3 +24,25 @@ def tokens_to_text(
     return text
 
 
+
+
+def generate_text(
+        raw_text,
+        model,
+        device,
+        look_back=3,
+        num_tokens_to_generate=20,
+):
+    starting_tokens = text_to_tokens(raw_text).to(device)
+
+    model.eval()
+    with torch.no_grad():
+        for _ in range(num_tokens_to_generate):
+            starting_tokens = starting_tokens[:, -look_back:]
+            logits = model(starting_tokens)[:,-1,:] #logits of the last token
+            next_token_prob_distribution = torch.softmax(logits,dim=-1) 
+            token_predicted = torch.argmax(next_token_prob_distribution,dim=-1,keepdim=True) 
+            tokens = torch.concat([starting_tokens,token_predicted],dim=1) 
+            starting_tokens = tokens
+            
+    return tokens_to_text(tokens)
