613e533205a4b9fc751b7687c1ed293c814abb85
Hawar-Dzaee <hawardizayee@gmail.com>
2025-04-27 18:55:31 -0400

    cleaner loss function

--- L2_Advanced/GPT2/loss.py
+++ L2_Advanced/GPT2/loss.py
@@ -1,6 +1,4 @@
-from torch import nn 
+import torch.nn.functional as F
 
-
-def loss_fn(logits,targets,device):
-    logits,targets = logits.to(device),targets.to(device)
-    return nn.functional.cross_entropy(logits.flatten(0,1),targets.flatten())
+def loss_fn(logits, targets):
+    return F.cross_entropy(logits.flatten(0, 1), targets.flatten())
