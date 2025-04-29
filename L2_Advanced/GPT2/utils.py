5ed75ea0409778ee76380b8f73720fa9220977ea
Hawar-Dzaee <hawardizayee@gmail.com>
2025-04-29 19:10:51 -0400

    update generate text function

--- L2_Advanced/GPT2/utils.py
+++ L2_Advanced/GPT2/utils.py
@@ -32,6 +32,9 @@ def generate_text(
         device,
         look_back=256,
         num_tokens_to_generate=20,
+        temperature = 1.0, 
+        top_k = None,
+        eos_id = None 
 ):
     starting_tokens = text_to_tokens(text_to_generate).to(device)
 
@@ -40,8 +43,20 @@ def generate_text(
         for _ in range(num_tokens_to_generate):
             starting_tokens = starting_tokens[:, -look_back:]
             logits = model(starting_tokens)[:,-1,:] #logits of the last token
-            next_token_prob_distribution = torch.softmax(logits,dim=-1) 
-            token_predicted = torch.argmax(next_token_prob_distribution,dim=-1,keepdim=True) 
+
+            if top_k : 
+                top_values, _ = torch.topk(logits,top_k)
+                logits = torch.where(
+                    logits<top_values[:,-1],
+                    -torch.inf,
+                    logits
+                    )
+                logits = logits/(temperature+1e-7)
+                probs  = torch.softmax(logits,dim=-1) 
+                token_predicted = torch.multinomial(probs,num_samples=1)
+            else : 
+                token_predicted = torch.argmax(logits,dim=-1,keepdim=True) 
+
             tokens = torch.concat([starting_tokens,token_predicted],dim=1) 
             starting_tokens = tokens
             text = tokens_to_text(tokens)
