5eb0c79286fa5279ec205465fbdba498b43e780d
Hawar-Dzaee <hawardizayee@gmail.com>
2025-05-05 21:43:54 -0400

    completed  SPT

--- L2_Advanced/GPT2/utils.py
+++ L2_Advanced/GPT2/utils.py
@@ -14,11 +14,18 @@ def text_to_tokens(
         return tokens
 
 
-
 def tokens_to_text(
         tokens,
         tokenizer=tiktoken.get_encoding('gpt2'),
         ):
+    
+    if type(tokens) is not torch.Tensor:
+        tokens = torch.tensor(tokens)
+
+    # when we have -100 tokens in the output, we need to remove them
+    if any(tokens < 0):
+        tokens = tokens[:torch.argmin(tokens)]
+
     tokens = tokens.squeeze(0).tolist() # untensored
     text = tokenizer.decode(tokens)
     return text
