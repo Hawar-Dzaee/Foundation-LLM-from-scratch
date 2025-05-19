4b14569d5ca4c8352a33fc6ca3ed396799b391ea
Hawar-Dzaee <hawardizayee@gmail.com>
2025-05-19 18:18:48 -0400

    finished classification script

--- module/src/metrics.py
+++ module/src/metrics.py
@@ -4,10 +4,16 @@ def  cross_entropy(logits, targets):
     loss_fn = nn.CrossEntropyLoss()
     return loss_fn(logits.flatten(0, 1), targets.flatten())
 
+
+def accuracy(logits,targets):
+    return (logits.flatten(0,1).argmax(dim=1) == targets.flatten()).float().mean()
+
+
+
 def classification_loss(logits, targets):
     loss_fn = nn.CrossEntropyLoss()
     return loss_fn(logits[:, -1, :], targets)
 
 
-def accuracy(logits,targets):
-    return (logits.flatten(0,1).argmax(dim=1) == targets.flatten()).float().mean()
+def classification_accuracy(logits,targets):
+    return (logits[:,-1,:].argmax(dim=1) == targets).float().mean()
