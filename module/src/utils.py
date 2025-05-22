2961fb76be3656ab36445af738a9bd26ff0f98c9
Hawar-Dzaee <hawardizayee@gmail.com>
2025-05-22 19:52:43 -0400

    moving generated text out of the Trainer class

--- module/src/utils.py
+++ module/src/utils.py
@@ -33,4 +33,30 @@ def tokens_to_text(
     text = tokenizer.decode(tokens)
     return text
 
+def generate_text(model,config):
+        starting_tokens = text_to_tokens(config["text_to_generate"]).to(config['device'])
 
+        model.eval()
+        with torch.no_grad():
+            for _ in range(config["num_tokens_to_generate"]):
+                starting_tokens = starting_tokens[:, -config["look_back"]:]
+                logits = model(starting_tokens)[:,-1,:] #logits of the last token
+
+                if config["top_k"] : 
+                    top_values, _ = torch.topk(logits,config["top_k"])
+                    logits = torch.where(
+                        logits<top_values[:,-1],
+                        -torch.inf,
+                        logits
+                        )
+                    logits = logits/(config["temperature"]+1e-7)
+                    probs  = torch.softmax(logits,dim=-1) 
+                    token_predicted = torch.multinomial(probs,num_samples=1)
+                else : 
+                    token_predicted = torch.argmax(logits,dim=-1,keepdim=True) 
+
+                tokens = torch.concat([starting_tokens,token_predicted],dim=1) 
+                text = tokens_to_text(tokens)
+                text = text.replace("\n", " ")
+                starting_tokens = tokens
+        return text
